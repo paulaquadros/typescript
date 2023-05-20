f5c6c9ad6516270de0cbd3431cf346c9d6ef4fd1
Paula Quadros de Mendonça <paulaquadros@MacBook-Air-de-Paula.local>
2023-05-20 09:13:44 -0400

    teste

---
+++ build/teste.js
@@ -0,0 +1,9 @@
+"use strict";
+const constante = 2;
+let variavel;
+var outraVariavel;
+let souVariável = "feia";
+const funcao = function () {
+    return "oie";
+};
+console.log(funcao);
---
+++ src/teste.ts
@@ -0,0 +1,10 @@
+const constante: number = 2;
+let variavel;
+var outraVariavel;
+let souVariável = "feia";
+
+const funcao = function(){
+    return "oie"
+}
+
+console.log(funcao)
